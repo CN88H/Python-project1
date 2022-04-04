ef535e4d7643ced20296bd4c6dffb8307cca1328
CN88H <cheenou88@gmail.com>
2022-04-03 21:53:13 -0500

    Complition of all the codes

--- daytripgen.py
+++ daytripgen.py
@@ -8,7 +8,7 @@ restaurants = ["Mon Ami Gabi", "Buffet at Bellagio", "Gordon Ramsey Burger", "Bu
 
 entertainments = ["Grand Canyon", "Vegas Helicopter", "Hoover Dam", "ATV Tours", "Magic show"]
 
-print("Welcome to the day trip generator! If you are having problems deciding where to go, we got you cover.")
+print("Welcome to the day trip generator. If you're unsure to what you want to do for your vacation, we can create one for you.")
 
 valid_response = False
 
@@ -27,3 +27,61 @@ while valid_response == False:
         print("I'll try again!")
     else:
         print("Wrong response.")
+
+    t_response = False
+
+while t_response == False:
+
+    transportation = random.choice(mode_of_transportation)
+
+    print("We have seleted mode of transportation of " + transportation + "!")
+
+    t_cont = input("Do you like this method? Enter y/n: ")
+
+    if t_cont == "y":
+        print("Great! Let's continue!")
+        t_response = True
+    elif t_cont == "n":
+        print("I'll try again!")
+    else:
+        print("Wrong response.")
+
+
+r_response = False
+
+while r_response == False:
+
+    restaurant = random.choice(restaurants)
+
+    print("We have select the " + restaurant + " for you.")
+
+    r_cont = input("Is this a good place to eat for you? Enter y/n: ")
+
+    if r_cont == "y":
+        print("Great! Let's continue!")
+        r_response = True
+    elif r_cont == "n":
+        print("I'll try again!")
+    else:
+        print("Wrong response.")
+
+e_response = False
+
+while e_response == False:
+
+    entertainment = random.choice(entertainments)
+
+    print("We have selected " + entertainment + " for your entertainment.")
+
+    e_cont = input("Are you satified with this choice? Enter y/n")
+
+    if e_cont == "y":
+        print("Awesome! Let's review everything.")
+        e_response = True
+    elif e_cont == "n":
+        print("I'll try again!")
+    else:
+        print("Wrong response.")
+
+print("You will be heading to " + destination + " to enjoy your day at " + entertainment + " by " + transportation + ", and will have a nice diner at the " + restaurant + ".")
+
