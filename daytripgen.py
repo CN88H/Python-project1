b8416e18ca8505010bc1574970bae5de4b8f4297
CN88H <cheenou88@gmail.com>
2022-04-03 20:27:16 -0500

    First of the project to commit

---
+++ daytripgen.py
@@ -0,0 +1,9 @@
+import random
+
+destinations_list = ["Las Vegas", "Miami", "New York", "Orlando", "Atlanta", "San Francisco"]
+
+mode_of_transportation = ["Uber", "Car rentals", "Bus", "Bike rentals"]
+
+restaurants = ["Mon Ami Gabi", "Buffet at Bellagio", "Gordon Ramsey Burger", "Buffet at Wynn", "Carson Kitchen", "Yardbird"]
+
+entertainments = ["Grand Canyon", "Vegas Helicopter", "Hoover Dam", "ATV Tours", "Magic show"]
